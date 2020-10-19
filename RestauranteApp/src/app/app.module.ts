5b5c91af2b699fed4348c15fcfa68748e4e1ee5b
lu <asd>
2020-10-19 20:00:39 -0300

    agrego login funcional, falta cambiar fuentes, cambiar usuarios en hardcodeo y... alfa: cargar usuarios en db; gamma: background, logo con nuevas fuentes a utilizar

--- RestauranteApp/src/app/app.module.ts
+++ RestauranteApp/src/app/app.module.ts
@@ -12,12 +12,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { environment } from "../environments/environment";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule],
+    AngularFireAuthModule, FormsModule],
   providers: [
     StatusBar,
     SplashScreen,
