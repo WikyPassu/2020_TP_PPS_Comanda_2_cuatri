20df52866f88f8adf8e7745e6c3c7c3b015edf5a
lu <asd>
2020-10-19 14:00:53 -0300

    agrego pagina, falta cargar la base

--- RestauranteApp/src/environments/environment.ts
+++ RestauranteApp/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBZLnTlRNjY9_l_QoxMCyZg_lQFEo4sa7w",
+    authDomain: "pruebaapp-33751.firebaseapp.com",
+    databaseURL: "https://pruebaapp-33751.firebaseio.com",
+    projectId: "pruebaapp-33751",
+    storageBucket: "pruebaapp-33751.appspot.com",
+    messagingSenderId: "1030547096826",
+    appId: "1:1030547096826:web:d088e5b3944368c39b3543",
+    measurementId: "G-SMQ3BDPE20"
+  },
 };
 
 /*
