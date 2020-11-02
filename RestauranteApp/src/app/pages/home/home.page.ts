d8d97cfd14bc4346e5e9dea6bab5c53f8e20868f
Demian Murray <murraydemian@gmail.com>
2020-11-02 10:21:23 -0300

    Modifico el login 	-separo la traduccion de codigo de errores en una funcion aparte 	-agrego una funcion para traer la informacion del usuario 	-agrego presentacion de errores por medio de toast 	-agrego comentarios pertinentes 	-cambio el ruteo para que entregue informacion del usuario a los 	otros componentes
    Modifico el home
    	-reemplazo los ion-cards por divs estilizados porque funciona
    	mejor el posicionamiento de items

--- RestauranteApp/src/app/pages/home/home.page.ts
+++ RestauranteApp/src/app/pages/home/home.page.ts
@@ -2,11 +2,12 @@
  * Este home page es para los clientes, por el momento la unica actividad 
  * posible es escanear el QR para entrar a la lista de espera.
  */
-
-import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult } from '@ionic-native/barcode-scanner/ngx';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -20,16 +21,21 @@ export class HomePage implements OnInit {
   public email: string;
   public maximo: number = 4;
   public minimo: number = 1;
+  public acceso;
   private user: any;
+
 
   constructor(
     private scanner: BarcodeScanner,
     private router: Router,
     private route: ActivatedRoute,
+    private toast: ToastController,
+    private fire: AngularFirestore,
   ) { }
 
   ngOnInit() {
-    try{
+    this.tab = 'local';
+    /*try{
       this.user = JSON.parse(this.route.snapshot.paramMap.get('user'));
       
       if(this.user.perfil == 'cliente'){
@@ -41,19 +47,7 @@ export class HomePage implements OnInit {
       }
     } catch {
       this.router.navigate(['login']);
-    }
-    
-    /*this.route.paramMap.subscribe( (data) => {
-      let tipo = 'cliente';
-      console.log(data['user']);
-      if(tipo == 'cliente'){
-        this.cambiarTab('local');
-      } else if (tipo == 'supervisor' || tipo == 'dueño'){
-        this.router.navigate(['supervisor']);
-      } else {
-        this.router.navigate(['login']);
-      }
-    })*/
+    }*/
   }
 
   cambiarTab(tabName: string){
@@ -61,12 +55,26 @@ export class HomePage implements OnInit {
     setTimeout( () => {this.tab = tabName}, 1000)
   }
   escanear(foo){
-    //console.log(foo);
+    this.presentToast('Escaner');
+    let resultado: string = 'OqJvTwaXF5GvroLveLvQ';
+    this.fire.doc('codigos/' + resultado)
+    .get().subscribe( (data) => {
+      console.log(data.data() );
+    });
   }
   agregarComenzal(cantidad: number){
     let resultado = this.comenzales + cantidad;
     this.comenzales = resultado > this.maximo ? this.maximo : resultado < this.minimo ? this.minimo : resultado;
     console.log(this.comenzales);
   }
-
+  async presentToast(message) {
+    const toast = await this.toast.create({
+      message: message,
+      duration: 2000,
+      position: "top",
+      animated: true,
+      mode: "md",
+    });
+    toast.present();
+  }
 }
